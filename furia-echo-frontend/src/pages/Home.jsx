e09379e21d91fee276a5d7a05d5dd977b8ca8749
M4RINH0 <dmmartins.dm28@gmail.com>
2025-05-01 13:09:57 -0300

    introdução site atualizada

--- furia-echo-frontend/src/pages/Home.jsx
+++ furia-echo-frontend/src/pages/Home.jsx
@@ -1,38 +1,35 @@
 import { useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import intro from '../assets/intro.mp4';
 
 export default function Home() {
-  const videoRef = useRef(null); // Criando uma referência para o vídeo
+  const videoRef = useRef(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.playbackRate = 1.35; // Definindo a velocidade de reprodução para 1.5x
+      videoRef.current.playbackRate = 1.35;
     }
   }, []);
 
+  const handleVideoEnd = () => {
+    navigate('/chat');
+  };
+
   return (
-    <div className="min-h-screen flex flex-col bg-gray-900 text-white">
+    <div className="min-h-screen flex flex-col bg-black text-white">
       <video 
-        ref={videoRef} // Atribuindo a referência ao vídeo
-        className="absolute top-0 left-0 w-full h-full object-cover" 
+        ref={videoRef}
+        className="absolute top-0 left-0 w-full h-full object-cover"
         autoPlay 
-        loop 
-        muted
+        muted 
+        onEnded={handleVideoEnd}
       >
         <source src={intro} type="video/mp4" />
         Seu navegador não suporta o elemento de vídeo.
       </video>
-      <div className="flex-grow flex flex-col justify-between relative z-10">
-        <h1 className="text-6xl font-extrabold mb-6 text-center mt-6 font-serif">Furia Echo</h1>
-        <div className="flex justify-center mb-6">
-          <Link 
-            to="/chat" 
-            className="px-10 py-5 bg-[#1a1a2e] hover:bg-[#0f0f1a] rounded-full font-medium text-white"
-          >
-            Entrar no Chat
-          </Link>
-        </div>
+      <div className="flex-grow flex flex-col justify-center items-center relative z-10">
+        <h1 className="text-6xl font-extrabold text-center font-serif">Furia Echo</h1>
       </div>
     </div>
   );
